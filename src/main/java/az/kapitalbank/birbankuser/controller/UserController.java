72b11044a62a118d917aa61ccff7605c50579511
unknown <aliagayev95@gmail.com>
2022-03-07 23:00:37 +0400

    prepare util package

--- src/main/java/az/kapitalbank/birbankuser/controller/UserController.java
+++ src/main/java/az/kapitalbank/birbankuser/controller/UserController.java
@@ -2,9 +2,11 @@ package az.kapitalbank.birbankuser.controller;
 
 import az.kapitalbank.birbankuser.domain.User;
 import az.kapitalbank.birbankuser.service.UserService;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -33,5 +35,12 @@ public class UserController {
     public User creatUser(@RequestBody User user) {
         return userService.createUser(user);
     }
-
+    @PutMapping("/users/{id}")
+    public User changeUser(@PathVariable Long id, @RequestBody User user){
+    return userService.changeUser(id,user);
+    }
+    @DeleteMapping("/users/{id}")
+    public void deleteUser(@PathVariable Long id){
+        userService.deleteUser(id);
+    }
 }
